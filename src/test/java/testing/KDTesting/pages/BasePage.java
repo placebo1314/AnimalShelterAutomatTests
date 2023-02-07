64cf9231fde647efe6f104fb07b1df84cecb58db
Krisz <placebo1314@gmail.com>
2023-02-07 21:30:50 +0100

    bugfixed utils path

--- src/test/java/testing/KDTesting/pages/BasePage.java
+++ src/test/java/testing/KDTesting/pages/BasePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import testing.pages.utils.DriverSingleton;
+import testing.KDTesting.utils.DriverSingleton;
 
 import java.time.Duration;
 import java.util.List;
